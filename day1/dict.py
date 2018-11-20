c0dc97f04e824d6d4b7e4a5df017a023dc9bd8ef
axindeng <axindeng@qq.com>
2018-11-20 16:21:06 +0800

    upload

---
+++ day1/dict.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python 
+d = { "x":1,"y":2,"z":3 }
+for key in d:
+    print(key,'match to',d[key])
---
+++ day1/while.py
@@ -0,0 +1,14 @@
+'''
+name = ''
+while not name:
+    name = input('Please enter your name:')
+print('hello,{}!'.format(name))
+'''
+
+num = ''
+for num in range(1,100):
+    print(num)
+
+num = [1,2,3,4,5,6,7,8,9,0]
+for n in num:
+    print(n)
