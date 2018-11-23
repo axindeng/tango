c48c911ac54b5b4dd53fad729f1b7c86260f94f5
axindeng <axindeng@qq.com>
2018-11-23 17:32:15 +0800

    added 第2章 控制流

---
+++ day3/exercise13.py
@@ -0,0 +1,7 @@
+#for i in range(1,11):
+#    print('No.' + str(i))
+
+i = 1
+while i < 11:
+    print('No.' + str(i))
+    i = i + 1
---
+++ day3/exercise9.py
@@ -0,0 +1,8 @@
+spam = 2
+
+if spam == 1:
+    print('Hello,you typed 1')
+elif spam == 2:
+    print('Howdy,you typed 2')
+else:
+    print('Greetings!,Now exit')
