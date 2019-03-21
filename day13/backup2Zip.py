b58437959f8ed020d4d731382b39e67373c2de71
axindeng <axindeng@qq.com>
2019-03-21 16:38:54 +0800

    upload

--- day13/backup2Zip.py
+++ day13/backup2Zip.py
@@ -3,4 +3,32 @@
 # Author: dengg@cmschina.com.cn
 ##########################################
 
-print('test')
+import os,zipfile
+def backup2zip(folder):
+    #todo
+#1.确定备份文件名的编号
+    folder = os.path.abspath(folder)
+    number = 1
+    while True:
+        zipFileName = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if  os.path.exists(zipFileName):
+            number = number + 1
+        else:
+            break
+    
+#2.使用os.walk进行递归压缩
+    backupZipfile = zipfile.ZipFile(zipFileName,'w')
+    for foldername,subfolders,filenames in os.walk(folder):
+        print('Adding files in %s....' % (foldername) )
+        backupZipfile.write(foldername)
+        for file in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if file.startswith(newBase) and file.endswith('.zip'):
+                continue
+            backupZipfile.write(os.path.join(foldername,file))
+    backupZipfile.close()
+    
+    print('Done.')
+    print(zipFileName)
+backup2zip('E:\\python\\tango')
+backup2zip('E:\\docker')
